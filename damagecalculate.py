e68f7efbe0d07f90e4bcf733fa87608c8cd0ab77
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-06-18 23:59:02 +0545

    Update damagecalculate.py

--- damagecalculate.py
+++ damagecalculate.py
@@ -15,7 +15,7 @@ def calculate_damage(damage, speed, time_unit):
 
 # Examples
 print(calculate_damage(40, 5, "second"))  
-print(calculate_damage(100, 1, "minute"))  # ➞ 6000
+print(calculate_damage(100, 1, "minute"))  
 print(calculate_damage(2, 100, "hour"))  
     time_units_in_seconds = {
         "second": 1,
