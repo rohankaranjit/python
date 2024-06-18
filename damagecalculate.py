fea69f47bbab9abcae937bcbadd313a33830e59a
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-06-18 23:58:38 +0545

    Update damagecalculate.py'

--- damagecalculate.py
+++ damagecalculate.py
@@ -1,1 +1,37 @@
+def calculate_damage(damage, speed, time_unit):
+    time_units_in_seconds = {
+        "second": 1,
+        "minute": 60,
+        "hour": 3600
+    }
+    
+    if time_unit not in time_units_in_seconds:
+        return "Invalid time unit"
+    
+    total_seconds = time_units_in_seconds[time_unit]
+    total_damage = damage * speed * total_seconds
+    
+    return total_damage
 
+# Examples
+print(calculate_damage(40, 5, "second"))  # ➞ 200
+print(calculate_damage(100, 1, "minute"))  # ➞ 6000
+print(calculate_damage(2, 100, "hour"))  # ➞ 720000def calculate_damage(damage, speed, time_unit):
+    time_units_in_seconds = {
+        "second": 1,
+        "minute": 60,
+        "hour": 3600
+    }
+    
+    if time_unit not in time_units_in_seconds:
+        return "Invalid time unit"
+    
+    total_seconds = time_units_in_seconds[time_unit]
+    total_damage = damage * speed * total_seconds
+    
+    return total_damage
+
+# Examples
+print(calculate_damage(40, 5, "second"))  # ➞ 200
+print(calculate_damage(100, 1, "minute"))  # ➞ 6000
+print(calculate_damage(2, 100, "hour"))  # ➞ 720000
