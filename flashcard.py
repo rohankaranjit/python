90ce1052752c35d30711c8d355e46c9b8e71a54d
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-06-28 23:55:25 +0545

    Update flashcard.py

--- flashcard.py
+++ flashcard.py
@@ -1,1 +1,9 @@
 
+class flashcard:
+    def __init__(self, word, meaning):
+        self.word = word
+        self.meaning = meaning
+    def __str__(self):
+       
+        #we will return a string 
+        return self.word+' ( '+self.meaning+' )'
