5d69b83657f9911b6961f918e41e4799355a212c
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-07-01 20:49:03 +0545

    Update inverted_pattern.py

--- inverted_pattern.py
+++ inverted_pattern.py
@@ -3,3 +3,6 @@ def inverted_full_pyramid(n):
 
         for j in range(n - i):
             print(" ", end="")
+            for k in range(2*i - 1):
+            print("*", end="")
+        print("")
