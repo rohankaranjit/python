1b1eee81ecc90fc1e5118a1413f058479dc0509b
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-07-04 23:46:57 +0545

    Update swap_comma_dots.py

--- swap_comma_dots.py
+++ swap_comma_dots.py
@@ -1,1 +1,6 @@
+# Python code to replace, with . and vice-versa
+def Replace(str1):
+	maketrans = str1.maketrans
+	final = str1.translate(maketrans(',.', '.,', ' '))
+	return final.replace(',', ", ")
 
