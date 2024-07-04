75601b33e68948e6198135d37c2e2d004ef3a6b4
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-07-04 23:49:02 +0545

    Update split_and_join.py

--- split_and_join.py
+++ split_and_join.py
@@ -3,6 +3,10 @@ def split_string(string):
 	list_string = string.split(' ')
 	
 	return list_string
+def join_string(list_string):
 
-
+	# Join the string based on '-' delimiter
+	string = '-'.join(list_string)
+	
+	return string
 
