fc5f258d367339e7ee2ab5fe0fd9fd38d36b1d88
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-07-05 23:57:02 +0545

    Update length_string.py

--- length_string.py
+++ length_string.py
@@ -16,5 +16,20 @@ def findLen(str):
         counter += 1
     return counter
  
-str = "geeks"
+str = "rohan"
 print(findLen(str))
+
+# Python code to demonstrate string length 
+# using for loop
+
+# Returns length of string
+def findLen(str):
+	counter = 0
+	for i in str:
+		counter += 1
+	return counter
+
+
+str = "rohan"
+print(findLen(str))
+
