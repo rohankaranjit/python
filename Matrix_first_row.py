e0e7db1120c58c361fe2c13683f74a2658eaea91
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-07-06 23:53:43 +0545

    Update Matrix_first_row.py

--- Matrix_first_row.py
+++ Matrix_first_row.py
@@ -16,6 +16,13 @@ test_list = [[5, 8, 9], [2, 0, 9], [5, 4, 2], [2, 3, 9]]
  
 # printing original list
 print("The original list : " + str(test_list))
+
+# dict() to convert back to dict type
+# slicing and pairing using zip() and list slicing
+res = dict(zip(test_list[0], test_list[1:]))
+ 
+# printing result
+print("The Assigned Matrix : " + str(res))
  
 
  
