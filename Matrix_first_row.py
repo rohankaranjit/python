52997fba699a6df7449ce3ee2cc942098876695f
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-07-06 23:53:31 +0545

    Update Matrix_first_row.py

--- Matrix_first_row.py
+++ Matrix_first_row.py
@@ -11,4 +11,11 @@ test_list = [[5, 8, 9], [2, 0, 9], [5, 4, 2], [2, 3, 9]]
 print("The original list : " + str(test_list))
 
 #Another one
+# initializing list
+test_list = [[5, 8, 9], [2, 0, 9], [5, 4, 2], [2, 3, 9]]
+ 
+# printing original list
+print("The original list : " + str(test_list))
+ 
+
  
