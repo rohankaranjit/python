cc1a69c1e17446fbd040ac760248f9762e96a6c7
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-07-07 23:14:26 +0545

    Update matrix_subtract.py

--- matrix_subtract.py
+++ matrix_subtract.py
@@ -8,5 +8,9 @@ A = np.array([[1, 2], [3, 4]])
 # creating second matrix
 B = np.array([[4, 5], [6, 7]])
 
+print("Printing elements of first matrix")
+print(A)
+print("Printing elements of second matrix")
+print(B)
 
 
