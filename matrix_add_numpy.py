775bfa5c5a9664d9f83ff0f868a92760db990f95
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-07-09 23:50:23 +0545

    Update matrix_add_numpy.py

--- matrix_add_numpy.py
+++ matrix_add_numpy.py
@@ -5,6 +5,7 @@ y = numpy.array([[7, 8], [9, 10]])
 # using add() to add matrices
 print ("The element wise addition of matrix is : ")
 print (numpy.add(x,y))
+
 # using subtract() to subtract matrices
 print ("The element wise subtraction of matrix is : ")
 print (numpy.subtract(x,y))
